81b91ead39c80776fb561b868151294b03ee304e
Benjamin Block <bjblock@gmail.com>
2021-03-04 10:04:23 -0600

    fixes typo in comment

--- kelloggram.js
+++ kelloggram.js
@@ -66,7 +66,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
 // {
 //   id: 'abcdefg',
 //   username: 'brian',
-//   imageURL: 'https://images.unsplash.com/...',
+//   imageUrl: 'https://images.unsplash.com/...',
 //   likes: 12,
 //   comments: [
 //     { username: 'brian', text: 'i love tacos!' },
